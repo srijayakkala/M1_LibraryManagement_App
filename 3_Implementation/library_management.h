b4273e51e3e2e1540f60300e02e0d3e65de1dee4
srijayakkala <80316335+srijayakkala@users.noreply.github.com>
2022-02-11 21:04:46 +0530

    Add files via upload

---
+++ 3_Implementation/library_management.h
@@ -0,0 +1,13 @@
+#ifndef LIBRARY_MANAGEMENT_H_
+#define LIBRARY_MANAGEMENT_H_
+#include"library_management.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct library
+{
+   char bk_name[30];
+   char publisher[30];
+   float price;
+};
+#endif
